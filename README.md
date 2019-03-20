1d5b3af6505d32ea864cb7d93859fd3975e3d854
godcong <jumbycc@163.com>
2019-03-20 16:13:42 +0800

    add go.mod

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Calendar 日期对象接口
+```
+ chronos.New() //创建当前日期
+ chronos.New(time.Now()) //创建当前日期
+ chronos.New("2017/11/14 08:17") //创建指定日期
+```
 
 # Lunar 农历日期显示
-
-创建Lunar对象：
-> lunar:=NewLunar(calendar Calendar)  
-
-显示农历日期:  
-> lunar.Date()
-
+```
+chronos.New().Lunar() //获取月历
+```
 # Solar 公历日期显示
-
+```
+chornos.New().Solar() //获取日历
+```
 
 
--- lunar_test.go
+++ lunar_test.go
@@ -10,14 +10,17 @@ import (
 	"github.com/godcong/chronos"
 )
 
+// TestGetSolarTerm ...
 func TestGetSolarTerm(t *testing.T) {
-	//GetSolarTerm(time.Now(), 0)
+
 }
 
+// TestYearDays ...
 func TestYearDays(t *testing.T) {
-	//log.Println(YearDays(1900))
+
 }
 
+// TestGetDayString ...
 func TestGetDayString(t *testing.T) {
 	//log.Println(GetDayString(20))
 	//reader := transform.NewReader(strings.NewReader(, simplifiedchinese.GB18030.NewDecoder())
@@ -25,6 +28,7 @@ func TestGetDayString(t *testing.T) {
 	fmt.Println(strconv.Itoa(0x97783), strconv.Itoa(0x97bd0), strconv.Itoa(0x97c36), strconv.Itoa(0xb0b6f), strconv.Itoa(0xc9274), strconv.Itoa(0xc91aa))
 }
 
+// TestGetTerm ...
 func TestGetTerm(t *testing.T) {
 	for i := 1; i <= 24; i++ {
 		i := chronos.GetTermInfo(2018, i)
@@ -33,24 +37,29 @@ func TestGetTerm(t *testing.T) {
 
 }
 
+// TestGetZodiac ...
 func TestGetZodiac(t *testing.T) {
 	log.Println(chronos.GetZodiac(time.Now()))
 }
 
+// TestStemBranchYear ...
 func TestStemBranchYear(t *testing.T) {
 	log.Println(chronos.StemBranchYear(2017))
 
 }
 
+// TestStemBranchMonth ...
 func TestStemBranchMonth(t *testing.T) {
 	log.Println(chronos.StemBranchMonth(2017, 11, 14))
 }
 
+// TestStemBranchDay ...
 func TestStemBranchDay(t *testing.T) {
 	log.Println(chronos.StemBranchDay(2017, 11, 14))
 
 }
 
+// TestStemBranchHour ...
 func TestStemBranchHour(t *testing.T) {
 	log.Print(12, chronos.StemBranchDay(2018, 1, 12), "日")
 	for i := 0; i <= 23; i++ {
@@ -62,12 +71,15 @@ func TestStemBranchHour(t *testing.T) {
 	log.Print(14, chronos.StemBranchDay(2017, 11, 14), "日")
 	log.Println(8, chronos.StemBranchHour(2017, 11, 14, 8))
 }
+
+// TestNewLunar ...
 func TestNewLunar(t *testing.T) {
 	log.Print(chronos.New().Lunar().Date())
 }
 
+// TestCalculateLunar ...
 func TestCalculateLunar(t *testing.T) {
 	//log.Print("now: ", chronos.Solar2Lunar(time.Parse()))
 	log.Print(chronos.New("2017/11/14 08:17").Lunar().EightCharacter())
-	log.Print(chronos.New(time.Now()).Lunar().EightCharacter())
+	log.Print(chronos.New(time.Now()).LunarDate())
 }
