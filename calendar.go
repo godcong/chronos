402e62673f5ac15117b909cd12b226eba996e50c
godcong <jumbycc@163.com>
2022-06-24 20:29:20 +0800

    feat(v2): v2 version added

--- calendar.go
+++ calendar.go
@@ -4,19 +4,16 @@ import (
 	"time"
 )
 
-// DateFormat ...
-const DateFormat = "2006/01/02 15:04"
+// DefaultDateFormat ...
+const DefaultDateFormat = "2006/01/02 15:04"
 const LunarDateFormat = "2006/01/02"
 
 type calendar struct {
 	time time.Time
 }
 
-// Calendar ...
-type Calendar interface {
-	Lunar() *Lunar
-	Solar() *Solar
-	LunarDate() string
+func (c *calendar) Time() time.Time {
+	return c.time
 }
 
 //New can input three type of time to create the calendar
@@ -26,24 +23,60 @@ type Calendar interface {
 func New(v ...interface{}) Calendar {
 	var c Calendar
 	if v == nil {
-		return &calendar{time.Now()}
+		return ParseTime(time.Now())
 	}
+
 	switch vv := v[0].(type) {
-	case nil:
-		c = &calendar{time.Now()}
 	case string:
-		c = formatDate(vv)
+		c = parseDate(vv, v...)
 	case time.Time:
 		c = &calendar{vv}
+	default:
+		c = ParseTime(time.Now())
 	}
 	return c
 }
 
-func formatDate(s string) Calendar {
-	t, err := time.Parse(DateFormat, s)
-	if err != nil {
-		t = time.Now()
+func parseStringFormat(v interface{}) string {
+	f := DefaultDateFormat
+	df, ok := (v).(string)
+	if ok {
+		f = df
 	}
+	return f
+}
+
+func parseStringTime(v interface{}) string {
+	t := ""
+	df, ok := (v).(string)
+	if ok {
+		t = df
+	}
+	return t
+}
+
+func parseDate(t string, vv ...interface{}) *calendar {
+	if t == "" {
+		return &calendar{
+			time: time.Now(),
+		}
+	}
+	f := DefaultDateFormat
+	if len(vv) > 1 {
+		f = parseStringFormat(vv[1])
+	}
+	c := &calendar{
+		time: time.Now(),
+	}
+
+	tt, err := time.Parse(f, t)
+	if err == nil {
+		c.time = tt
+	}
+	return c
+}
+
+func ParseTime(t time.Time) Calendar {
 	return &calendar{
 		time: t,
 	}
@@ -51,7 +84,7 @@ func formatDate(s string) Calendar {
 
 // Lunar ...
 func (c *calendar) Lunar() *Lunar {
-	return CalculateLunar(c.time.Format(DateFormat))
+	return CalculateLunar(c.time.Format(DefaultDateFormat))
 }
 
 // Solar ...
