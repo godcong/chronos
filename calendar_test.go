0e33b0067e430e7d86fc29b00209504e0e4bae57
godcong <jumbycc@163.com>
2022-08-24 16:23:53 +0800

    feat(v2): v2 version added

---
+++ calendar_test.go
@@ -0,0 +1,52 @@
+package chronos
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	type args struct {
+		v []any
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{
+				v: nil,
+			},
+			want: time.Now().Format(DefaultDateFormat),
+		},
+		{
+			name: "",
+			args: args{
+				v: []any{
+					"2022/08/24 12:34",
+				},
+			},
+			want: "2022/08/24 12:34",
+		},
+		{
+			name: "",
+			args: args{
+				v: []any{
+					"2022-08-24 12:34",
+					"2006-01-02 15:04",
+				},
+			},
+			want: "2022/08/24 12:34",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(tt.args.v...); !reflect.DeepEqual(got.String(), tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
